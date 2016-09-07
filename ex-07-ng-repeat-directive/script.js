9258b538914f3a9937bf67b22088ab06c57e3e44
Dariusz Franczak <dariusz@3amp.com>
2016-09-07 11:37:50 +0100

    first commit

---
+++ ex-07-ng-repeat-directive/script.js
@@ -0,0 +1,16 @@
+
+
+var myApp = angular
+						.module('myModule', [])
+						.controller('myController', function ($scope) {
+
+																								var employee = {
+																									firstName: 'Ben',
+																									lastName: 'Hastings',
+																									gender: 'Male'
+																								};
+
+																									$scope.message = 'Hallo Angular!';
+																									$scope.employee = employee;
+
+																								});
