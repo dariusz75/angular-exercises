3a859e3f91a86aa3b51972be6c13bde97e88c012
Dariusz Franczak <dariusz@3amp.com>
2016-09-07 15:14:28 +0100

    end of lesson

--- ex-07-ng-repeat-directive/script.js
+++ ex-07-ng-repeat-directive/script.js
@@ -4,13 +4,70 @@ var myApp = angular
 						.module('myModule', [])
 						.controller('myController', function ($scope) {
 
-																								var employee = {
-																									firstName: 'Ben',
-																									lastName: 'Hastings',
-																									gender: 'Male'
-																								};
+																					var employees = [ 
+																														{ firstName: 'Ben', lastName: 'Hastings', gender: 'Male',salary: 5500 },
+																														{ firstName: 'John', lastName: 'Smith', gender: 'Male', salary: 6500 },
+																														{ firstName: 'Ana', lastName: 'Tylor', gender: 'Female', salary: 7500 },
+																														{ firstName: 'Josh', lastName: 'Maker', gender: 'Male', salary: 8500 },
+																														{ firstName: 'Sofia', lastName: 'Cool', gender: 'Female', salary: 9500 },
+																													];
 
-																									$scope.message = 'Hallo Angular!';
-																									$scope.employee = employee;
+																					var countrie = [
+																														{
+																															name: 'UK',
+																															cities: [
+																																				{name: 'London'},
+																																				{name: 'Bristol'},
+																																				{name: 'Leeds'},
+																																			]
+																														},
+																														{
+																															name: 'Poland',
+																															cities: [
+																																				{name: 'Warsaw'},
+																																				{name: 'Krakow'},
+																																				{name: 'Poznan'},
+																																			]
+																														},
+																														{
+																															name: 'Germany',
+																															cities: [
+																																				{name: 'Berlin'},
+																																				{name: 'Hamburgh'},
+																																				{name: 'Frankfurt'},
+																																			]
+																														},
+																													];
 
-																								});
+																							var countries = [
+																														{
+																															name: 'UK',
+																															cities: [
+																																				{name: 'London'},
+																																				{name: 'Bristol'},
+																																				{name: 'Leeds'},
+																																			]
+																														},
+																														{
+																															name: 'Poland',
+																															cities: [
+																																				{name: 'Warsaw'},
+																																				{name: 'Krakow'},
+																																				{name: 'Poznan'},
+																																			]
+																														},
+																														{
+																															name: 'Germany',
+																															cities: [
+																																				{name: 'Berlin'},
+																																				{name: 'Hamburgh'},
+																																				{name: 'Frankfurt'},
+																																			]
+																														},
+																													];
+
+
+																					$scope.employees = employees;
+																					$scope.countries = countries;
+
+											});
