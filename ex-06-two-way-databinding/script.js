a2bd5171bc9c9618783a496e5af6d22fd5ef40b4
Dariusz Franczak <dariusz@3amp.com>
2016-09-07 11:26:08 +0100

    end of lesson

--- ex-06-two-way-databinding/script.js
+++ ex-06-two-way-databinding/script.js
@@ -4,6 +4,13 @@ var myApp = angular
 						.module('myModule', [])
 						.controller('myController', function ($scope) {
 
+																								var employee = {
+																									firstName: 'Ben',
+																									lastName: 'Hastings',
+																									gender: 'Male'
+																								};
+
 																									$scope.message = 'Hallo Angular!';
+																									$scope.employee = employee;
 
 																								});
