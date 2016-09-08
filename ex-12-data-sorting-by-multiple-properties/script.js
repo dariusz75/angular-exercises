cfa5f4a68fa4515db0ab24062697e0461ab4fde7
Dariusz Franczak <dariusz@3amp.com>
2016-09-08 12:47:39 +0100

    end of lesson

--- ex-12-data-sorting-by-multiple-properties/script.js
+++ ex-12-data-sorting-by-multiple-properties/script.js
@@ -7,31 +7,36 @@ var myApp = angular
 												var employees = [
 													{
 														name: 'Ben',
-														dateOfBirth: new Date('November 23, 1999'), 
+														dateOfBirth: new Date('November 23, 1999'),
+														city: 'London', 
 														gender: 'Male',
 														salary: 22222.959696,
 													},
 													{
 														name: 'Bill',
 														dateOfBirth: new Date('September 13, 1989'), 
+														city: 'Moscow', 
 														gender: 'Male',
 														salary: 33333.76777,
 													},
 													{
 														name: 'Josh',
 														dateOfBirth: new Date('June 23, 1993'), 
+														city: 'Berlin', 
 														gender: 'Male',
 														salary: 44444.6666,
 													},
 													{
 														name: 'Andrew',
 														dateOfBirth: new Date('January 13, 2000'), 
+														city: 'Warsaw', 
 														gender: 'Male',
 														salary: 55555.099,
 													},
 													{
 														name: 'John',
 														dateOfBirth: new Date('March 13, 2000'), 
+														city: 'Paris', 
 														gender: 'Male',
 														salary: 66666.999,
 													},
