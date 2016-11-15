06fa86b06aa390093276d37799ac73fa56788949
Dariusz Franczak <darius32@op.pl>
2016-11-15 00:44:54 +0000

    08 completed

---
+++ udemy/08-routing/scripts/app.js
@@ -0,0 +1,45 @@
+/* W celu uzycia routingu musimy poinformowac Angulara deklarujac ngRoute module w zaleznosciach modulu. */
+var myApp = angular.module('myApp', ['ngRoute']);
+
+
+
+
+/* Nastepnie musimy skonfigurowac aplikacje za pomoca metody config 
+i okreslic nasz */
+myApp.config(function ($routeProvider) {
+
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	})
+	.when('/first', {
+		templateUrl: 'pages/template1.html',
+		controller: 'firstController'
+	})
+	.when('/second', {
+		templateUrl: 'pages/template2.html',
+		controller: 'secondController'
+	})
+	.when('/third', {
+		templateUrl: 'pages/template3.html',
+		controller: 'thirdController'
+	})
+});
+
+myApp.controller('mainController', ['$scope', function($scope) {	
+	$scope.message = "This message comes from the Main Controller.";
+}]);
+
+myApp.controller('firstController', ['$scope', function($scope) {
+	$scope.message = "This message comes from the First Controller.";
+}]);
+
+myApp.controller('secondController', ['$scope', function($scope) {
+	$scope.message = "This message comes from the Second Controller.";
+}]);
+
+myApp.controller('thirdController', ['$scope', function($scope) {
+	$scope.message = "This message comes from the Third Controller.";
+}]);
