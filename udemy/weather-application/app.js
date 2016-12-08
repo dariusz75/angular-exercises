2edd24e0aecdc28358799d0d8a2befb6807fd458
Dariusz Franczak <darius32@op.pl>
2016-12-08 00:19:27 +0000

    some improvements done

--- udemy/weather-application/app.js
+++ udemy/weather-application/app.js
@@ -13,6 +13,10 @@ weatherApp.config(function ($routeProvider) {
 		templateUrl: 'pages/forecast.html',
 		controller: 'forecastCtrl'
 	})
+	.when('/forecast/:days', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastCtrl'
+	})
 
 });
 
@@ -40,11 +44,22 @@ weatherApp.controller('forecastCtrl', ['$scope', '$resource', '$routeParams', 'c
 
 	$scope.city = cityService.city;
 
+	$scope.days = $routeParams.days || '2';
+
 	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=4c7b31de1420a31ef8d22efaab2b0161", 
 	{ callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
 
-	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
-	console.log($scope.weatherResult);
+	$scope.convertToCelsius = function(kelvins) {
+
+		return Math.round(kelvins - 273);
+
+	}
+
+	$scope.convertToDate = function(date) {
+
+		return new Date(date * 1000);
+	}
 
 }]);
