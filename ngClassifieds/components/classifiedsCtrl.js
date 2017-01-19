48a1dd84b31daf56a7417dab02339b9393b3a163
Dariusz Franczak <[darius32@op.pl]>
2017-01-19 18:09:51 +0000

    side slider added

--- ngClassifieds/components/classifiedsCtrl.js
+++ ngClassifieds/components/classifiedsCtrl.js
@@ -2,7 +2,31 @@
 	'ese strict';
 
 	angular.module('ngClassifieds')
-	.controller('classifiedsCtrl', function($scope) {
-		$scope.message = "Hello again!!!!!";
+	.controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
+
+		classifiedsFactory.getClassifieds().then(function(classifieds){
+			$scope.classifieds = classifieds.data
+		});
+
+		var contact = {
+			name: 'Dariusz Franczak',
+			phone: '07707636696',
+			email: 'darius32@op.pl'
+		}
+
+		$scope.openSidebar = function() {
+			$mdSidenav('left').open();
+		}
+
+		$scope.closeSidebar = function() {
+			$mdSidenav('left').close();
+		}
+
+		$scope.saveClassified = function(classified) {
+			$scope.classified.contact = contact;
+			$scope.classifieds.push(classified);
+			$scope.classified = {};
+		}
+
 	});
 })();
