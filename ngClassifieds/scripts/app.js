e21e8c5e622635baf5871913f42ef91f4c0c6362
Dariusz Franczak <[darius32@op.pl]>
2017-01-24 15:28:04 +0000

    Search functionality added

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -1,10 +1,21 @@
 angular
-	.module('ngClassifieds', ['ngMaterial'])
-	.config(function($mdThemingProvider) {
+	.module('ngClassifieds', ['ngMaterial', 'ui.router'])
+	.config(function($mdThemingProvider, $stateProvider) {
 
 		$mdThemingProvider.theme('default')
 			.primaryPalette('teal')
 			.accentPalette('orange');
+
+		$stateProvider
+			.state('stateone', {
+				url: '/stateone',
+				template: '<h1>State one!</h1>'
+			})
+			.state('statetwo', {
+				url: '/statetwo',
+				template: '<h1>State two!</h1>'
+			});
+
 	})
 	.directive("helloWorld", function() {
 		return {
