ff3c0799cd6e5f5c9fb60f9f1b87cecfbe76df72
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-10-25 11:00:55 +0100

    ex-06 corrections

--- ex-06-two-way-databinding/script.js
+++ ex-06-two-way-databinding/script.js
@@ -11,6 +11,6 @@ var myApp = angular
 																								};
 
 																									$scope.message = 'Hallo Angular!';
-																									$scope.employee = employee;
+																									$scope.employeeExample = employee;
 
 																								});
