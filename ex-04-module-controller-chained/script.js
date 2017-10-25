91f7d494dd980e376de6584aeba708a78f2da9ab
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-10-25 10:35:58 +0100

    ex-04 comments added

--- ex-04-module-controller-chained/script.js
+++ ex-04-module-controller-chained/script.js
@@ -1,4 +1,27 @@
+/*
+We can chain the module with the controller so instead of this:
 
+var myApp = angular.module('myModule', []);
+
+myApp.controller('registeredController', function ($scope) {
+	
+	var person = {
+		firstName: 'John',
+		surname: 'Smith',
+		gender: 'male'
+	};
+
+	$scope.myPerson = person;
+	$scope.firstMessage = 'Angular Tutorial - first message';
+	$scope.secondMessage = 'Some text - second message';
+	$scope.thirdMessage = 'End of Angular Tutorial - third message'
+
+
+});
+
+We can do this:
+
+*/
 
 var myApp = angular
 						.module('myModule', [])
