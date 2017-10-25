ff36a45dba862348efcbde597f238bc3ae3408d2
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-10-25 13:40:21 +0100

    ex-09 corrections

--- ex-09-filters/script.js
+++ ex-09-filters/script.js
@@ -12,9 +12,9 @@ var myApp = angular
 														salary: 22222.959696,
 													},
 													{
-														name: 'Bill',
+														name: 'Claire',
 														dateOfBirth: new Date('November 13, 1989'), 
-														gender: 'Male',
+														gender: 'Female',
 														salary: 33333.76777,
 													},
 													{
@@ -28,6 +28,30 @@ var myApp = angular
 														dateOfBirth: new Date('November 13, 2000'), 
 														gender: 'Male',
 														salary: 55555.099,
+													},
+													{
+														name: 'John',
+														dateOfBirth: new Date('November 23, 1999'), 
+														gender: 'Male',
+														salary: 66662.959696,
+													},
+													{
+														name: 'Andrew',
+														dateOfBirth: new Date('November 13, 1989'), 
+														gender: 'Male',
+														salary: 77777.76777,
+													},
+													{
+														name: 'Josh',
+														dateOfBirth: new Date('November 23, 1993'), 
+														gender: 'Male',
+														salary: 88888.6666,
+													},
+													{
+														name: 'Betty',
+														dateOfBirth: new Date('November 13, 2000'), 
+														gender: 'Female',
+														salary: 99999.099,
 													},
 												];
 
