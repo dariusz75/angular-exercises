a19e558af611ed855dd5b1520e9531b3e6758672
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-10-26 11:41:16 +0100

    some old folders removed

--- ex-16-services/script.js
+++ ex-16-services/script.js
@@ -4,28 +4,24 @@ var myApp = angular
 						.module('myModule', [])
 						.controller('myController', function ($scope) {
 												
-												$scope.transformString = function (input) {
-													
-													if (!input) {
-														return input;
-													} 
-														var output = '';
+																					$scope.transformString = function (input) {
+																																		
+																																		var output = '';
+
+																																		if (!input) {
+																																			return input;
+																																		} 
+
+																																		for (var i = 0; i < input.length; i++) {
+																																			if (i > 0 && input[i] === input[i].toUpperCase()) {
+																																				output = output + ' ';
+																																			} 
+																																				output = output + input[i];															
+																																		}
+																																			
+																																		$scope.output = output;
+																																		
+																									}
+																							});
 
 
-														for (var i = 0; i < input.length; i++) {
-															if (i > 0 && input[i] === input[i].toUpperCase() ) {
-																output = output + ' ';
-															} 
-																output = output + input[i];															
-														}
-														
-												$scope.output = output;
-												}
-											});
-
-
-/*
-Aby utworzyc service w Angularze wystarczy utworzyc key o dowolnej nazwie (w naszym przypadku transformString) 
-i przypisac go do obiektu $scope.
-
-*/
